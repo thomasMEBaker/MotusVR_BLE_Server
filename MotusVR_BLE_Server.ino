f2cb99459ad8b46e319d8b0fde22bec21c5af2d0
Tom Baker <thomasmebaker@gmail.com>
2023-09-12 16:55:58 +0100

    small changes

--- MotusVR_BLE_Server.ino
+++ MotusVR_BLE_Server.ino
@@ -8,15 +8,10 @@
 To do -
 Fix issue with connection on PC
 Sort out gamepad movement and range of scale
-Battery service
 Change to dynamically changing mac address name
 Connection and disconnection - move into classes and BLEServerCallbacks
-
-  BLEService *pBatteryService = hid->batteryService();
-  BLECharacteristic *pBatteryLevelCharacteristic = pBatteryService->createCharacteristic(BATTERY_LEVEL_UUID,BLECharacteristic::PROPERTY_READ);
-  uint8_t initialBatteryLevel = 50; // Example: 50%
-  pBatteryLevelCharacteristic->setValue(&initialBatteryLevel, 1);
-  pBatteryService->start();
+Implement BNO080
+Kalman filter of values
 */
 
 #define LED_BUILTIN 2
@@ -105,12 +100,38 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("Starting BLE");
-  setupGamepadBLE();
+  //setupGamepadBLE();
+  setupExampleCode();
 }
 
 void loop() {
- toggleAButton();
+ //toggleAButton();
  delay(100);
+}
+
+void setupExampleCode(){
+ Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  BLEDevice::init("Long name works now");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                          CHARACTERISTIC_UUID,
+                                          BLECharacteristic::PROPERTY_READ |
+                                          BLECharacteristic::PROPERTY_WRITE
+                                        );
+
+  pCharacteristic->setValue("Hello World says Neil");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void setupGamepadBLE(){
